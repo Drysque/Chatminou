de3d74f7dbd9ed18e62ab5ed1f54c9bcce7d62e5
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2022-05-10 01:36:34 +0200

    Add win and rank graphs to profile page

--- front/src/pages/auth/Profile.tsx
+++ front/src/pages/auth/Profile.tsx
@@ -1,59 +1,62 @@
-import {
-	Box,
-	Center,
-	Spinner,
-	Table,
-	TableCaption,
-	TableContainer,
-	Tbody,
-	Td,
-	Tr,
-	Image,
-	VStack,
-} from '@chakra-ui/react';
+import { useEffect, useMemo } from 'react';
+import { Center, Spinner, Image, VStack, Text, HStack, Tooltip, Badge, Progress, useToast } from '@chakra-ui/react';
 
 import { useAppSelector } from 'services/hooks';
-import { useGetAccountQuery } from 'services/henrikApi/account';
+import { useGetAccountQuery, useGetMMRQuery } from 'services/henrikApi/account';
+import ranks from 'services/utils/ranks.json';
+
+import { WinRate, Wins } from 'components/data/Wins';
+import { Rank } from 'components/data/Rank';
 
 export const Profile = (): JSX.Element => {
 	const tagline = useAppSelector((state) => state.auth.tagline);
 	const { data: account } = useGetAccountQuery(tagline!, { skip: !tagline });
+	const toast = useToast();
 
-	if (!account) return <Spinner />;
+	const { data: mmr, isError: isMMRError } = useGetMMRQuery(tagline!, { skip: !tagline });
 
-	console.log(account.card);
+	useEffect(() => {
+		isMMRError && toast({ title: 'Error', description: 'Could not fetch MMR', status: 'error' });
+	}, [isMMRError, toast]);
+
+	const current_rank = useMemo(
+		() => ranks.reverse()[0].tiers.find((tier) => tier.tier === mmr?.current_data.currenttier)!,
+		[mmr],
+	);
+
+	if (!account || !mmr) return <Spinner />;
 
 	return (
 		<Center>
-			<VStack m="64px" bg="chatminou.bg" p="16px 32px" border="base" borderRadius="16px">
-				<TableContainer>
-					<Table variant="simple" colorScheme="black">
-						<TableCaption placement="top">Your account information</TableCaption>
-						<Tbody>
-							<Tr>
-								<Td>Username</Td>
-								<Td>{account.name}</Td>
-							</Tr>
-							<Tr>
-								<Td>Tag</Td>
-								<Td>{account.tag}</Td>
-							</Tr>
-							<Tr>
-								<Td>Unique User ID</Td>
-								<Td>{account.puuid}</Td>
-							</Tr>
-							<Tr>
-								<Td>Account Level</Td>
-								<Td>{account.account_level}</Td>
-							</Tr>
-							<Tr>
-								<Td>Region</Td>
-								<Td>{account.region}</Td>
-							</Tr>
-						</Tbody>
-					</Table>
-				</TableContainer>
-				<Image w="100%" src={account.card.wide} />
+			<VStack w="100%">
+				<VStack m="64px" align="start">
+					<HStack w="100%">
+						{current_rank.smallIcon && (
+							<Tooltip label={current_rank.tierName}>
+								<Image src={current_rank.smallIcon} maxH="40px" />
+							</Tooltip>
+						)}
+						<Progress
+							value={mmr.current_data.ranking_in_tier}
+							w="100%"
+						/>
+						<Text fontSize="14px">{mmr.current_data.ranking_in_tier}/100</Text>
+					</HStack>
+					<HStack>
+						<Tooltip label={account.puuid}>
+							<HStack spacing="0px" align="baseline">
+								<Text fontSize="22px">{account.name}</Text>
+								<Text fontSize="14px">#{account.tag}</Text>
+							</HStack>
+						</Tooltip>
+						<Badge variant="solid">{account.account_level}</Badge>
+						<Badge variant="outline">{account.region}</Badge>
+					</HStack>
+					<Image w="100%" src={account.card.wide} />
+				</VStack>
+				<Rank />
+				<Wins />
+				<WinRate />
 			</VStack>
 		</Center>
 	);
