49849082e759187e8d33af70fd8ba63bedd4aa27
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2022-05-15 15:00:42 +0200

    Add httpsAgent to constants

--- back/src/utils/constants.ts
+++ back/src/utils/constants.ts
@@ -1,6 +1,8 @@
+import { Agent } from 'https';
+
 export const appConstants = {
   port: 8080,
-} as const;
+};
 
 export const riotConstants = {
   accessToken: {
@@ -14,5 +16,15 @@ export const riotConstants = {
   entitlements: {
     url: 'https://entitlements.auth.riotgames.com/api/token/v1',
   },
-  user_agent: 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)',
-} as const;
+  userAgent: 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)',
+  httpsAgent: new Agent({
+    ciphers: [
+      'TLS_CHACHA20_POLY1305_SHA256',
+      'TLS_AES_128_GCM_SHA256',
+      'TLS_AES_256_GCM_SHA384',
+      'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256',
+    ].join(':'),
+    honorCipherOrder: true,
+    minVersion: 'TLSv1.2',
+  }),
+};
